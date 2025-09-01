1050bbb1122ed89e7a067442f3236b5b44bedb08
AddiSchwirtlich <addischwirtlich@gmail.com>
2025-09-01 21:49:52 +1200

    changed song
    changed song that's really it

--- music_workinprogress.js
+++ music_workinprogress.js
@@ -9,42 +9,20 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
-   let circleY = 50;
-   let circleSize = 70;
-   let circleSize2 = 30;
-
+ 
 // changes 
    // vocal bar is red
-   //fill(200, 0, 0);
-   //rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   //fill(0);
-   //text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+   fill(200, 0, 0);
+   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+   fill(0);
+   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
    // drum bar is green
-   //fill(0, 200, 0);
-   //rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   //fill(0);
-   //text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
-
-   //fill(255,0,0);
-
-   let vocalMap = map(vocal,0,100,0,50);
-   let r = map(vocal,0,100,0,255);
-   let b = map(vocal,0,100,255,0);
-
-   let drumMap = map(drum,0,100,0,50);
-   let g = map(drum,0,100,255,0);
-   let d = map(drum,0,100,0,255);
-
-   for(let i = 1; i <6; i = i +1.2){
-      fill(r,0,b);
-      let circleZ = 50 + (110*i);
-      ellipse(300,circleZ,circleSize + vocalMap);
-      fill(0,g,d);
-      ellipse(300,circleZ,circleSize2 + drumMap);
-   }
-
-
+   fill(0, 200, 0);
+   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+   fill(0);
+   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+ 
    // bass bar is blue
    fill(50, 50, 240);
    rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
