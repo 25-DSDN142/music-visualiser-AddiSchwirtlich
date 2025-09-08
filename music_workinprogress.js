77063bfd4dcb812ccf7782829a23f45b4b60a88e
AddiSchwirtlich <addischwirtlich@gmail.com>
2025-09-08 19:24:43 +1200

    finally got loop of animation working:)
    made loop for running animation

--- music_workinprogress.js
+++ music_workinprogress.js
@@ -1,45 +1,67 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
+let firstRun = true
+
+let Snail = [];
+let imageIndex = 0;
+let frameDelay = 5;
+let frameCounter = 0;
+
+
+
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  //if (firstRun) {
+    //rectMode(CENTER);
+    //Snail.push(loadImage('Run_Frame1.png'));
+    //Snail.push(loadImage('Run_Frame2.png'));
+    //Snail.push(loadImage('Run_Frame3.png'));
+    //Snail.push(loadImage('Run_Frame4.png'));      
+    //Snail.push(loadImage('Run_Frame5.png'));
+    //Snail.push(loadImage('Run_Frame6.png'));
+    //Snail.push(loadImage('Run_Frame7.png'));
+    //Snail.push(loadImage('Run_Frame8.png'));
+    //Snail.push(loadImage('Run_Frame9.png'));
+    //Snail.push(loadImage('Run_Frame10.png'));
+    //Snail.push(loadImage('Run_Frame11.png'));
+    //Snail.push(loadImage('Run_Frame12.png'));
+
+
+    //firstRun = false
+  //}
+
+
+  background(5);
+  rectMode(CENTER);
+
+ for (let i = 1; i < 12; i++)  {
+      Snail.push(loadImage('Run_Frame' + i + '.png'));
+  }
+
+
+
+if (Snail.length > 0) {
+    image(Snail[imageIndex], width/110, height/140);
+  }
+
+  frameCounter++;
+  if (frameCounter >= frameDelay) {
+    imageIndex++;
+    if (imageIndex >= Snail.length) {
+      imageIndex = 0;
+    }
+    frameCounter = 0;
+  }
+
+  frameDelay = map(drum, 0, 100, 10, 2);
+
+  //var VocalFrame = int(map(vocal, 0,100, 0,11));
+
+  //console.log(VocalFrame);
+  //push();
+  //scale(0.5);
+  //image(Snail[VocalFrame], width/2, height/2)
+  //pop();
 
    
    
