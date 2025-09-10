941a00d5b20954331dbfa56e467f16ee829b7baf
AddiSchwirtlich <addischwirtlich@gmail.com>
2025-09-10 17:35:42 +1200

    added more ellipses
    played around with the idea of adding a waveform but was too difficult for me to do so made a bunch of ellipses am thinking of making like an eye shape

--- music_workinprogress.js
+++ music_workinprogress.js
@@ -1,24 +1,30 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 
-let Run = [];
+let Run = []; // array for holding running animation
 let imageIndex = 0;
 let frameDelay = 25; // frame delay of running animation
 let frameCounter = 0;
 
-let firstRun = true;
-
+let firstRun = true; // used for run animation
 
 let palette = [   // colour palette for background colours
   [20,20,54],
   [16,35,48],
   [31,25,54],
-  [31,132,250],
+  [181, 13, 13],
   [26,20,69]
 ];
 
 let ellipseVocal = 100; // used for map of sizing of ellipse
 let ellipseBass = 100; // used for map of bass for ellipse
+let ellipseDrum = 100; // used for map of drum of ellipse
+
+let eyeWidth = 100; // 
+let eyeHeight = 100; //
+
+let irisWidth = 100;
+let irisHeight = 100;
 
 let b = 30; //  used for map of blue
 let r = 30; //  used for map of red
@@ -26,47 +32,71 @@ let r = 30; //  used for map of red
 let b2 = 30; // used for drum map of blue
 let r2 = 30; // used for drum map of red
 
+
+
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
 
 if (drum > 50) {  // adjust threshold 0–100
     let c = random(palette);
-    background(c[0], c[1], c[2]);
+    background(c[0], c[1], c[2]); //  uses my colours from palette
   } else {
     background(0,0,69); // default dark background
   }
 
 
-  rectMode(CENTER);
-
 if (firstRun) { // loop run of running animation
  for (let i = 1; i < 12; i++)  {
       Run.push(loadImage('run/Run_Frame' + i + '.png'));
   }
-  firstRun = false;
+  firstRun = false; // used to not crash from loading too many images :)
 
 }
-  noStroke();
-  fill(r2,0,b2);
-  ellipse(250,400,ellipseBass,ellipseBass);
+  stroke(r2,0,b2);
+  strokeWeight(5);
+  noFill();
+  ellipse(250,200,ellipseBass,ellipseBass);
+  ellipse(250,200,ellipseBass-30,ellipseBass-30);
+  ellipse(250,200,ellipseBass-60,ellipseBass-60) // large ellipse
 
-  fill(r,0,b);
-  ellipse(250,400,ellipseVocal,ellipseVocal);
+  stroke(r,0,b);
+  ellipse(250,200,ellipseVocal,ellipseVocal);
+  ellipse(250,200,ellipseVocal-30,ellipseVocal-30);
+  ellipse(250,200,ellipseVocal-60,ellipseVocal-60); // medium ellipse
+
+  stroke(255);
+  ellipse(250,200,ellipseDrum,ellipseDrum);
+  ellipse(250,200,ellipseDrum-30,ellipseDrum-30);
+  ellipse(250,200,ellipseDrum-60,ellipseDrum-60);// small ellipse
 
   fill(255);
-  ellipse(250,400,50,50);
+  noStroke();
+  ellipse(250,200,eyeWidth,eyeHeight); // size of eye
 
-  ellipseVocal = map(vocal, 0, 100, 80, 150);
-  ellipseBass = map(bass, 0, 100, 150, 210);
+  fill(r2,0,b2);
+  ellipse(250,200,irisWidth,irisHeight); // size of iris
 
-  b = map(other, 0, 100, 0, 250);
-  r = map(other, 100, 0, 0, 250);
+
+  ellipseVocal = map(vocal, 0, 100, 30, 220);
+  ellipseBass = map(bass, 0, 100, 30, 210);
+  ellipseDrum = map(drum, 0, 100, 30, 330);
+
+  eyeWidth = map(vocal, 0, 100, 5, 20);
+  eyeHeight = map(vocal, 0, 100, 10, 90);
+
+  irisWidth = map(bass, 0, 100, 2, 10);
+  irisHeight = map(bass, 0, 100, 9, 70);
+
+  b = map(other, 0, 100, 0, 250); // map for blue colour of medium ellipse
+  r = map(other, 100, 0, 0, 250); // map for red colour of medium ellipse
 
   b2 = map(drum, 100, 0, 0, 250);
   r2 = map(drum, 0, 100, 0 , 250);
 
+
 if (Run.length > 0) {
-    image(Run[imageIndex], width/110, height/140);
+    image(Run[imageIndex], width/110, height/140); // width and height can be used to move run animation
   }
 
   frameCounter++;
@@ -80,7 +110,10 @@ if (Run.length > 0) {
 
   frameDelay = map(bass, 0, 100, 10, 2);  // changes how fast or slow run animation is based off of drum
 
-  //ellipse(250,300,100,100);
-  
    
 }
+
+
+
+
+
